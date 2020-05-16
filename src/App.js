95a527f409c5d95335b15d06500c92f30b74aa59
uran224 <uran22@gmail.com>
2020-05-17 02:34:39 +0300

    Lesson 11

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://www.freelogodesign.org/Content/img/logo-samples/bakary.png' />
       </header>
+
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+
+      <div className='content'>
+
+        <div>
+        <img src='https://www.markgray.com.au/images/gallery/large/wanaka-tree.jpg' />
+        </div>
+
+        <div>
+          <img className='ava' src='https://www.marieclaire.com.au/media/43934/maya-angelou.jpg?width=720&center=0.0,0.0' />
+          
+          description
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+            <div>
+              post 3
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
