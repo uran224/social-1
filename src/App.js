c9cb181b478581edca82924d9f5496ff85776eca
uran224 <uran22@gmail.com>
2020-05-17 20:06:43 +0300

    Lesson 19. Profile-Messages switching
    npm install react-router-dom -save

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />
+        </div>
       </div>
-      {/* <Profile /> */}
+    </BrowserRouter>
 
-    </div>
   );
 }
 
