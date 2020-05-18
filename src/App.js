f2341f8b2212fab2b311b6f701db8df01fdcfd19
uran224 <uran22@gmail.com>
2020-05-18 20:42:10 +0300

    Lesson 19. Howework News-Music-Settings
    route, Browser-Router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
 import { Route, BrowserRouter } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 
 
@@ -17,6 +20,9 @@ const App = () => {
         <div className='app-wrapper-content'>
           <Route path="/profile" component={Profile} />
           <Route path="/dialogs" component={Dialogs} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
         </div>
       </div>
     </BrowserRouter>
---
+++ src/components/News/News.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './News.module.css';
+
+const News = (props) => {
+    return (
+        <div>
+            Newsssssss
+        </div>
+    );
+}
+
+export default News;
