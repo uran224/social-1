877108afdd9e355fd17f6c988e9030b2f931f0e7
uran224 <uran22@gmail.com>
2020-05-18 21:45:59 +0300

    Lesson 20. No page reloading, Navbar activeLink

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = () => {
         <div>
 
             <div>
-                <img src='https://www.markgray.com.au/images/gallery/large/wanaka-tree.jpg' />
+                <img src='https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg' />
             </div>
 
             <div>
