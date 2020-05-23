e3f01a99ee92ebbf37dbd0901178343b29cc990a
uran224 <uran22@gmail.com>
2020-05-23 08:49:35 +0300

    Lesson 27. Component vs render
    Прокидываем данные через Route

--- src/App.js
+++ src/App.js
@@ -18,8 +18,13 @@ const App = () => {
         <Header />
         <Navbar />
         <div className='app-wrapper-content'>
-          <Route path="/profile" component={Profile} />
-          <Route /*exact*/ path="/dialogs" component={Dialogs} />
+          {/* <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogs} />  */}
+          {/* what is the "exact path" */}
+          
+          <Route path="/profile" render={ () => <Profile /> } />
+          <Route path="/dialogs" render={ () => <Dialogs /> } /> 
+          
           <Route path="/news" component={News} />
           <Route path="/music" component={Music} />
           <Route path="/settings" component={Settings} />
