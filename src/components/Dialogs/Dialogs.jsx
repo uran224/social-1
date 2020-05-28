a337cd4398f0f2d94ed7bbf8fc3c74e1d34a7059
uran224 <uran22@gmail.com>
2020-05-28 05:12:07 +0300

    Lesson 31. Homework. Input area added to Dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,18 +9,37 @@ const Dialogs = (props) => {
     let dialogsElements = props.state.dialogs.map( d  => <DialogItem id={d.id} name={d.name} /> );
     let messagesElements = props.state.messages.map( m => <Message text = {m.text} />);
 
+    let newMessageElement = React.createRef();
+
+    let addMessage = () =>{
+        let text = newMessageElement.current.value;
+        alert (text);
+    }
+
     return (
-        <div className={s.dialogs}> 
+        <div className={s.dialogs}>
             <div className={s.dialogsItems}>
 
-                { dialogsElements }
+                {dialogsElements}
 
             </div>
             <div className={s.messages}>
+                <div>
+                    <div>
+                        <textarea ref={newMessageElement} ></textarea>
+                    </div>
+                    <div>
+                        <button onClick={addMessage} >Add message</button>
+                    </div>
+                </div>
+                <div>
 
-                { messagesElements }
-         
+                    {messagesElements}
+
+                </div>
+
             </div>
+
         </div>
     );
 }
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,19 +7,19 @@ const MyPosts = (props) => {
     let postsElements = 
         props.posts.map( p => <Post message={p.message} likesCount={p.likesCount}/>);
 
-    let newPostelement = React.createRef();
+    let newPostElement = React.createRef();
 
     let addPost = () =>{
-        let text = newPostelement.current.value;
+        let text = newPostElement.current.value;
         alert (text);
     }
 
     return (
         <div className={s.postBlock}>
-            <h3>My posts</h3>
+            <h3>My crazy posts</h3>
             <div>
                 <div>
-                   <textarea ref={newPostelement} ></textarea>
+                   <textarea ref={newPostElement} ></textarea>
                 </div>
                 <div>
                    <button onClick={ addPost } >Add post</button>
