71c78520603c9a21b3f3426dcc12402c2b5017bb
uran224 <uran22@gmail.com>
2020-05-28 05:55:14 +0300

    Lesson 32. Прокидываем callback через props. Почему не видим новый пост?

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import state from './redux/state';
+import {addPost} from './redux/state';
+import {BrowserRouter} from "react-router-dom"
 
+// addPost ('ERERERER');
 
-
-ReactDOM.render(<App state={state}  />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App state={state} addPost={addPost} /> 
+    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
