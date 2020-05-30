a0de85af0bd4c033f29a9b11d3b19ef3ae7498be
uran224 <uran22@gmail.com>
2020-05-30 15:55:25 +0300

    38. dispatch, action
    Сокращаем число методов, теперь меняем store через dispatch

--- src/App.js
+++ src/App.js
@@ -27,8 +27,7 @@ const App = (props) => {
           <Route path='/profile'
             render={ () => <Profile 
               profilePage={props.state.profilePage} 
-              addPost={props.addPost} 
-              updateNewPostText={props.updateNewPostText}
+              dispatch={props.dispatch} 
             /> } />
 
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -32,36 +32,37 @@ let store = {
             // for homework
         }
     },
-    getState() {
-        return this._state;
-    },
     _callSubscriber() {
         console.log('State has changed');
     },
-    addPost() {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0
-        };
-    
-        this._state.profilePage.posts.push(newPost);
-        this._state.profilePage.newPostText = '';
-        this._callSubscriber(this._state);
-    
-    },
-    updateNewPostText(newText) {
-        this._state.profilePage.newPostText = newText;
-        this._callSubscriber(this._state);
+
+    getState() {
+        return this._state;
     },
     subscribe(observer) {
         this._callSubscriber = observer; // observer  // addEventListener
+    },
+
+    dispatch(action) { // { type: 'ADD-POST' }
+        if (action.type === 'ADD-POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+        
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state);
+        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state);
+        }
     }
 
 }
 
-
-
 export default store;
+
 window.state = store;
 // store - OOP
