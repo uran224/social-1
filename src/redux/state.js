e33e593bb016e2e91d60b4c9d1fc4717f563181a
uran224 <uran22@gmail.com>
2020-05-30 14:59:33 +0300

    37. store, state, OOP, refactoring code
    вынос мозга продолжается по экспоненте

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,65 +1,67 @@
-let rerenderEntireTree = () => {
-    console.log('State has changed');
-}
-
-let state = {
-    profilePage: {
-        posts: [ 
-            {id: 1, message:"Hey, how are you getting on, asshole?", likesCount: "100500"},
-            {id: 2, message:"This is the second post!", likesCount: "20"},
-            {id: 3, message:"Asshole!", likesCount: "10"},
-            {id: 4, message:"This is the fdsfdssd", likesCount: "111"} 
-        ],
-        newPostText: 'Type smth, asshole'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [ 
+                {id: 1, message:"Hey, how are you getting on, asshole?", likesCount: "100500"},
+                {id: 2, message:"This is the second post!", likesCount: "20"},
+                {id: 3, message:"Asshole!", likesCount: "10"},
+                {id: 4, message:"This is the fdsfdssd", likesCount: "111"} 
+            ],
+            newPostText: 'Type smth, asshole'
+        },
+    
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Jim'},
+                {id: 2, name: 'Marta-Marta'},
+                {id: 3, name: 'Peter II'},
+                {id: 4, name: 'Carol'},
+                {id: 5, name: 'Ursula'}
+            ],
+    
+            messages: [
+                {id: 1, text:'Hi, how are you!'},
+                {id: 2, text:'Hi, shit asshole!'},
+                {id: 3, text:'How are you getting on?'},
+                {id: 4, text:'Yo'},
+                {id: 5, text:'Yo'}
+            ]
+        },
+    
+        sidebar: {
+            // for homework
+        }
     },
-
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Jim'},
-            {id: 2, name: 'Marta-Marta'},
-            {id: 3, name: 'Peter II'},
-            {id: 4, name: 'Carol'},
-            {id: 5, name: 'Ursula'}
-        ],
-
-        messages: [
-            {id: 1, text:'Hi, how are you!'},
-            {id: 2, text:'Hi, shit asshole!'},
-            {id: 3, text:'How are you getting on?'},
-            {id: 4, text:'Yo'},
-            {id: 5, text:'Yo'}
-        ]
+    getState() {
+        return this._state;
     },
-
-    sidebar: {
-        // for homework
+    _callSubscriber() {
+        console.log('State has changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+    
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; // observer  // addEventListener
     }
-}
-
-window.state = state;
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree();
 
 }
 
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree();
 
-}
 
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; // observer  // addEventListener
-
-}
-
-export default state;
+export default store;
+window.state = store;
+// store - OOP
