377cbdeb93e318aaa36fb8ec1b55ed65aec0f3b0
uran224 <uran22@gmail.com>
2020-06-07 21:29:07 +0300

    49. Создаём страницу пользователей Users.jsx
    Посмотреть логику срабатывания кнопки Follow-Unfollow

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
+import UsersContainer from './components/Users/UsersContainer';
 
 
 
@@ -25,6 +26,10 @@ const App = (props) => {
             render={ () => <DialogsContainer  /> } />
           <Route path='/profile'
             render={ () => <Profile /> } />
+
+          <Route path='/users'
+            render={ () => <UsersContainer /> } />
+
 
 
           <Route path='/news' component={News} />
