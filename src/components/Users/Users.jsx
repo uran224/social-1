9677f9a601bc836281e7da3aebb841fcc7575959
uran224 <uran22@gmail.com>
2020-06-09 23:37:29 +0300

    51. Users API - делаем get-запрос на сервер, получаем список users
    Установили библиотеку axios. Дебажил почему список юзеров странный, оказалось,
    он живой и пополняется такими же нубами, как я.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,75 +1,56 @@
 import React from 'react';
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/197509.jpg'
 
 let Users = (props) => {
 
-    if (props.users.length === 0) (
-        props.setUsers( [
-            {
-                id: 1,
-                photoUrl: 'https://c1.staticflickr.com/6/5202/5250519868_82f526213c_b.jpg',
-                followed: false,
-                fullname: 'Chloi',
-                status: 'I am boss',
-                location: { city: 'Minsk', country: 'Belarus' }
-            },
-            {
-                id: 2,
-                photoUrl: 'https://thumbs.gfycat.com/TenderLeanHuia-mobile.jpg',
-                followed: true,
-                fullname: 'Cherem',
-                status: 'I am boss-2',
-                location: { city: 'Moscow', country: 'Russia' }
-            },
-            {
-                id: 3,
-                photoUrl: 'https://3.bp.blogspot.com/-Xh6MW4hhlh4/XDX0KAvQDsI/AAAAAAACguc/qpeMxtVgZLUHzRe9jSsRaU7KVnk2UfasACLcBGAs/s1600/Ashley-Graham-Marina-Rinaldi-Spring-2019-Campaign01.jpg',
-                followed: false,
-                fullname: 'Marina',
-                status: 'I am boss-3',
-                location: { city: 'Kiev', country: 'Ukrain' }
-            }
-        ]
-        )
-    )
+    if (props.users.length === 0) {
 
-    
-
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+        });
+    }
+  
     return <div>
         {
-            props.users.map( u => <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photoUrl} className={styles.userPhoto}  />
-                    </div>
-                    <div>
-                        {u.followed 
-                            ? <button onClick={() => ( props.unfollow(u.id) ) } >Unfollow</button> 
-                            : <button onClick={() => ( props.follow(u.id) ) } >Follow</button> 
-                        }                      
-                    </div>
-                </span>
-                <span>
+            props.users.map(u =>
+                <div key={u.id}>
                     <span>
                         <div>
-                            {u.fullName}
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                         </div>
                         <div>
-                            {u.status}
+                            {u.followed
+                                ? <button onClick={() => (props.unfollow(u.id))} >Unfollow</button>
+                                : <button onClick={() => (props.follow(u.id))} >Follow</button>
+                            }
                         </div>
                     </span>
                     <span>
-                        <div>
-                            {u.location.country}
-                        </div>
-                        <div>
-                            {u.location.city}
-                        </div>
+                        <span>
+                            <div>
+                                {u.name}
+                            </div>
+                            <div>
+                                {u.id}
+                                {u.status}
+                            </div>
+                        </span>
+                        <span>
+                            <div>
+                                {"u.location.country"}
+                            </div>
+                            <div>
+                                {"u.location.city"}
+                            </div>
+                        </span>
+
                     </span>
 
-                </span>
-
-            </div> )
+                </div>
+            )
         }
     </div>
 }
