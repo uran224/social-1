a120fed2317b2539851520ca1592ca682a6c37bd
uran224 <uran22@gmail.com>
2020-06-11 00:56:30 +0300

    53. Вместо функциональной, ввели классовую компоненту Users (extends ReactComponent)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,60 +3,66 @@ import styles from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assets/images/197509.jpg'
 
-let Users = (props) => {
+class Users extends React.Component {
 
-    let getUsers = () => {
-        if (props.users.length === 0) {
-
+    constructor (props) {
+        super(props);
+        //alert('NEW');
+ 
             axios.get("https://social-network.samuraijs.com/api/1.0/users")
                 .then(response => {
-                    props.setUsers(response.data.items);
+                    this.props.setUsers(response.data.items);
             });
-        }
+ 
 
     }
 
-    return <div>
-        <button onClick = {getUsers}>Get Users</button>
-        {
-            props.users.map(u =>
-                <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
-                        </div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => (props.unfollow(u.id))} >Unfollow</button>
-                                : <button onClick={() => (props.follow(u.id))} >Follow</button>
-                            }
-                        </div>
-                    </span>
-                    <span>
+ 
+
+    render() {
+        return <div>
+            {/* <button onClick={this.getUsers}>Get Users</button> */}
+            {
+                this.props.users.map(u =>
+                    <div key={u.id}>
                         <span>
                             <div>
-                                {u.name}
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} />
                             </div>
                             <div>
-                                {u.id}
-                                {u.status}
+                                {u.followed
+                                    ? <button onClick={() => (this.props.unfollow(u.id))} >Unfollow</button>
+                                    : <button onClick={() => (this.props.follow(u.id))} >Follow</button>
+                                }
                             </div>
                         </span>
                         <span>
-                            <div>
-                                {"u.location.country"}
-                            </div>
-                            <div>
-                                {"u.location.city"}
-                            </div>
+                            <span>
+                                <div>
+                                    {u.name}
+                                </div>
+                                <div>
+                                    {u.id}
+                                    {u.status}
+                                </div>
+                            </span>
+                            <span>
+                                <div>
+                                    {"u.location.country"}
+                                </div>
+                                <div>
+                                    {"u.location.city"}
+                                </div>
+                            </span>
+
                         </span>
 
-                    </span>
+                    </div>
+                )
+            }
+        </div>
+    }
 
-                </div>
-            )
-        }
-    </div>
 }
 
 
