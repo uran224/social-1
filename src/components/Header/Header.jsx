00172663f82bf0e5d882e392a22ae12eaa5bbdc1
uran224 <uran22@gmail.com>
2020-06-19 21:00:24 +0300

    61. Cookie, login в теории, auth me
    Заработало как надо, когда залогинился на social-netwwork.samurai-js.com

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import s from './Header.module.css';
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <img className={s.img} src='https://www.freelogodesign.org/Content/img/logo-samples/bakary.png' />
+
+            <div className={s.loginBlock} >
+                { props.isAuth ? props.login
+                    :  <NavLink to={'/login'}>Login</NavLink> }
+            
+            </div>
         </header>
     );
 }
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -10,7 +10,7 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
         if (!userId) {
-            userId = 8;
+            userId = 2;
         }
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
             .then(response => {
